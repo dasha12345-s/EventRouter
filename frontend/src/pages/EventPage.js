007cfd6ffb76ba20ddf9eb8c6bf373c54cdc7403
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-04-13 11:36:36 -0400

    Data fetching with a loader

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -1,26 +1,37 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
-const PRODUCTS = [
-  {id: 'event1', title: 'MY FIRST EVENT'},
-  {id: 'event2', title: 'MY FIRST EVENT'},
-  {id: 'event3', title: 'MY FIRST EVENT'},
-]
+import EventsList from '../components/EventsList';
 
-function EventPage(){
-  return(
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/events');
+
+      if (!response.ok) {
+        setError('Fetching events failed.');
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
+  return (
     <>
-    <h1> all Events </h1>
-    <ul>
-      {PRODUCTS.map((eve) => (
-        <li key={eve.id}>
-          <Link to={`/events/${eve.id}`}>
-            {eve.title}
-            </Link>
-        </li>
-      ))}
-    </ul>
+      <div style={{ textAlign: 'center' }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
-  )
+  );
 }
 
-export default EventPage;
+export default EventsPage;
