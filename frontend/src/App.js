79e6deb56d74e2b9daa8e1d481d3b0178726da0a
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-04-13 11:57:43 -0400

    uusing data from the loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,17 @@ const router = createBrowserRouter([
       { path: 'events', 
         element: <RootEventPage/>,
         children: [
-          { index: true, element: <EventPage/>},
+          { index: true, 
+            element: <EventPage/>, 
+            loader: async () => {
+            const response = await fetch('http://localhost:8080/events');
+
+            if (!response.ok) {
+            } else {
+              const resData = await response.json();
+              return resData.events;
+            }
+          }},
           { path: ':eventsId', element: <EventDetailPage/> },
           { path: 'new', element: <NewEventPage/> },
           { path: ':eventsId/edit', element: <EditEventPage/> },
