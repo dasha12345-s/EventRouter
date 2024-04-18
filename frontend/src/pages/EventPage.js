79bb239eddaf7b4a4a9a08f2fd452dfdad87576c
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-04-18 12:18:14 -0400

    Deferrring Data Fetching with useFetccher()

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -1,21 +1,22 @@
-import { useLoaderData, json } from 'react-router-dom';
-
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
+import {Suspense} from 'react'
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
+  const {events} = useLoaderData();
 
-  // if (data.isError){
-  //   return <p>{data.message}</p>
-  // }
-  const events = data.events;
-
-  return <EventsList events={events}/>;
+  return(
+    <Suspense fallback={<p style={{textAlign: 'center'}}> Loading...</p>}>
+    <Await resolve={events}>
+    {(loadEvenys) => <EventsList events={loadEvenys}/> }
+  </Await>
+  </Suspense>
+  ) 
 }
 
 export default EventsPage;
 
-export async function loader(){
+async function loadEvenys(){
   const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
@@ -26,6 +27,13 @@ export async function loader(){
         {message: 'Could not fetch events.'},
         {status: 500,});
     } else {
-      return response;
+      const resData = await response.json();
+      return resData.events;
     }
+}
+
+export function loader(){
+  return defer({
+    events: loadEvenys()
+  })
   }
